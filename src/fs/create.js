be1f43c2daad08016e1dafad1a156effecd92534
Nectire <yazykovmaxim@gmail.com>
2022-06-01 17:28:35 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
+import { F_OK } from "fs/constants"; 
+import {writeFile, access} from 'fs/promises' 
+
 export const create = async () => {
-    // Write your code here 
+    try {
+        const dir = __dirname + "files/";
+        const filename  = 'fresh.txt';
+        console.log(dir);
+        const res = await access(dir + filename, F_OK);
+        console.log(res, ' acces');
+        const content = "I am fresh and young";
+        writeFile(dir + filename, content);
+    } catch (error) {
+        console.error(error);
+    }
 };

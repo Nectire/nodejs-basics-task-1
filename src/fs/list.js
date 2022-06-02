9f0d40525de6435acd1992acfa539fea86d698cb
Nectire <yazykovmaxim@gmail.com>
2022-06-02 22:03:05 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,22 @@
+import * as fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const list = async () => {
-    // Write your code here 
+  const pathTofiles = join(__dirname, "files");
+
+  try {
+    if (!fs.existsSync(pathTofiles)) throw new Error("FS operation failed");
+
+    fs.readdir(pathTofiles, {withFileTypes: true}, (err, files) => {
+      if (err) throw new Error(err);
+    files.forEach(file => console.log(file.name));
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+list();

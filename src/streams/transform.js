adbb852bdc59891da437909871b55296642e28af
Nectire <yazykovmaxim@gmail.com>
2022-06-04 19:09:11 +0300

    feat: implement transform function with pipeline

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,32 @@
+import { Transform, pipeline } from "stream";
+
+class CustomTransformStream extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, callback) {
+    try {
+      const resultString = chunk.toString("utf-8").split("").reverse().join("");
+      callback(null, resultString);
+    } catch (err) {
+      callback(err);
+    }
+  }
+}
 export const transform = async () => {
-    // Write your code here 
+    const transformStream = new CustomTransformStream();
+    pipeline(
+        process.stdin,
+        transformStream,
+        process.stdout,
+        (err) => {
+        if (err) {
+            console.error("Pipeline failed.", err);
+          }
+        }
+
+    )
 };
+
+transform();

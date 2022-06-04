cae0f90ff0b37bc77d53137b768ed5709dbfc641
Nectire <yazykovmaxim@gmail.com>
2022-06-04 18:36:12 +0300

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,16 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const regex = /^RSS_.+$/;
+    try {
+        const arrOfEnvVars =[];
+        Object.keys(process.env).forEach((key) => {
+            if(key.match(regex)) {
+                arrOfEnvVars.push(`${key}=${process.env[key]}`);
+            }
+        });
+        console.log(arrOfEnvVars.join('; '));
+    } catch (error) {
+        console.error(error);
+    }
 };
+
+parseEnv();

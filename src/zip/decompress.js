a0dd12f4cbee8cf5785b8cb7792d10834134fa0a
Nectire <yazykovmaxim@gmail.com>
2022-06-05 11:42:39 +0300

    feat: implement decompress function with gunzip and streams api

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,24 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createGunzip } from "zlib";
+import { promisify } from "util";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const decompress = async () => {
-    // Write your code here 
+  const pipe = promisify(pipeline);
+  const input = join(__dirname, "files", "archive.gz");
+  const output = join(__dirname, "files", "fileToCompress.txt");
+  try {
+    const gzip = createGunzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipe(source, gzip, destination);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+decompress();

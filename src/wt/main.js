0411a5d60f50f5ea6fcc755223bc71140f0783cb
Nectire <yazykovmaxim@gmail.com>
2022-06-05 16:59:10 +0300

    feat: implement function with worker_threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,28 @@
+import { dirname, join, } from "path";
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const performCalculations = async () => {
-    // Write your code here
+  const pathToFile = join(__dirname, "worker.js");
+  const arrOfPromises = [];
+
+  const done = (el) => new Promise((resolve) => {
+    el.on("message", (obj) => resolve(obj));
+    el.on("error", () => resolve({ status: "rejected", value: null }));
+  });
+
+    cpus().forEach((cpu, i) =>{
+        const worker = new Worker(pathToFile);
+        worker.postMessage(10 + i);
+        arrOfPromises.push(done(worker));
+    })
+    const results = (await Promise.allSettled(arrOfPromises))
+        .map(object => object.value);
+
+    return console.log(results);
 };
+
+performCalculations();
